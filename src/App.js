fa288ec6ae9377e27a8e6dc9dcf974a0e65cdb4b
geno223 <eugenio.jimenez22397@gmail.com>
2024-12-14 10:47:14 -0500

    this is a component

--- src/App.js
+++ src/App.js
@@ -3,10 +3,10 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      Hello World
-    </div>
-  );
+<div>
+    <h1>Geno Jim</h1>
+    <h2>Hello World</h2>
+</div>
+  )
 }
-
 export default App;
